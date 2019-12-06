399ee087c3ef345a426733edf756df11b2305c27
youngjunSung <seong0814@in-comms.co.kr>
2019-12-06 18:00:26 +0900

    수정

--- babel-gulp/src/js/lib.js
+++ babel-gulp/src/js/lib.js
@@ -1,4 +1,4 @@
-// src/lib.js
+// src/js/lib.js
 let sum = (a, b) => a + b;
 
 console.log(sum(3, 4));
@@ -15,9 +15,4 @@ const say = ({ subject, verb, object }) => {
 // sentence 배열 내에 있는 각 객체별로 subject, verb, object를 연결해 출력
 for (let s of sentences) {
   say(s);
-}
-
-export function power(x, y) {
-  // ES7: 지수 연산자
-  return x ** y;
 }
